1eba4399c83a05c5f106bf2bfa6e8e9b7724ee05
Sven Le Cann <sven.lecann@live.fr>
2020-12-21 03:45:10 +0100

    Section 6 - 130

--- models/meal.js
+++ models/meal.js
@@ -1,7 +1,7 @@
 export class Meal {
   constructor(
     id,
-    categoryId,
+    categoryIds,
     title,
     affordability,
     complexity,
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View, FlatList } from 'react-native';
 import { CATEGORIES, MEALS } from '../data/dummy-data';
+import { MealItem } from '../components/MealItem';
 
 const CategoryMealsScreen = (props) => {
   const renderMealItem = (itemData) => {
     return (
-      <View>
-        <Text>{itemData.item.title}</Text>
-      </View>
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {}}
+      />
     );
   };
   const catId = props.navigation.getParam('categoryId');
 
   const displayedMeals = MEALS.filter(
-    (meal) => meal.categoryId.indexOf(catId) >= 0
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
   return (
     <View style={styles.screen}>
-      <FlatList data={displayedMeals} renderItem={renderMealItem} />
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        style={{ width: '100%' }}
+      />
     </View>
   );
 };
